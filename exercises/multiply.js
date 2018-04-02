5b5cc6d6f543690b4bed6b9edde40533197f9c33
Guillaume de Stéfano <g.destefano@orange.fr>
2018-04-02 18:41:48 +0200

    tdd2 fin

--- exercises/multiply.js
+++ exercises/multiply.js
@@ -10,6 +10,24 @@
 
 // Your code :
 
+function multiply(num1, num2) {
+  let sum = 0;
+  for (let i = 0; i < Math.abs(num2); i++) {
+    sum += num1;
+  }
+  if (num1 < 0 && num2 < 0) {
+    return Math.abs(sum);
+  } else if (num1 < 0 || num2 < 0 ) {
+    return -sum;
+  } else if (num1 < 0 && num2 > 0)  {
+    return -sum
+  } else {
+    return sum;
+  }
+}
+
+
+
 //* Begin of tests
 const assert = require('assert')
 
@@ -23,6 +41,6 @@ assert.strictEqual(multiply(123, 0), 0)
 assert.strictEqual(multiply(0, -230), 0)
 assert.strictEqual(multiply(0, 0), 0)
 assert.strictEqual(multiply(123, -22), -2706)
-assert.strictEqual(multiply(-22, 123), -2706)
+assert.strictEqual(multiply(-22, 123), -2706) 
 assert.strictEqual(multiply(-22, -123), 2706)
 // End of tests */
--- exercises/same-sign.js
+++ exercises/same-sign.js
@@ -9,6 +9,22 @@
 
 // Your code :
 
+function sameSign(x, y) {
+    if (x > 0 && y > 0) {
+        return true;
+    }else if (x < 0 && y < 0) {
+        return true;
+    }else if (x == 0 && y == 0) {
+        return true;
+    } else if (x > 0 || y > 0) {
+        return false;
+    }else if (x < 0 || y < 0) {
+        return false;
+    }
+}
+
+
+
 //* Begin of tests
 const assert = require('assert')
 
@@ -16,11 +32,11 @@ assert.strictEqual(typeof sameSign, 'function')
 assert.strictEqual(sameSign.length, 2)
 assert.notStrictEqual(sameSign, Math.sameSign)
 assert.strictEqual(sameSign(-2, -1), true)
-assert.strictEqual(sameSign(0, 0), true)
+assert.strictEqual(sameSign(0, 0), true)//
 assert.strictEqual(sameSign(12, 3232), true)
 assert.strictEqual(sameSign(1, -1), false)
 assert.strictEqual(sameSign(-231, 1), false)
 assert.strictEqual(sameSign(-231, 0), false)
-assert.strictEqual(sameSign(0, 231), false)
+assert.strictEqual(sameSign(0, 231), false) //
 assert.strictEqual(sameSign(231, -233), false)
 // End of tests */
