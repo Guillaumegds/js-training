49b2b5c5fe21f1d42dbe2a8a668df10ef00dadc7
Guillaume de Stéfano <g.destefano@orange.fr>
2018-04-24 15:36:04 +0200

    finished

--- exercises/jaden-case.js
+++ exercises/jaden-case.js
@@ -7,11 +7,22 @@
  * and return capitalize each words: "How are you ?" -> "How Are You ?"
  *
  */
-
-
+function jadenCase(string) {
+    let tableau = string.split(' ');
+       
+    for(let i= 0; i < tableau.length; i++){
+      let word = tableau[i].split('');
+      word[0] = word[0].toUpperCase();
+      tableau[i] = word.join('');
+    }
+        return tableau.join(' ');
+}
+  
 
 //* Begin of tests
 const assert = require('assert')
-
-assert.fail('You must write your own tests')
+assert.strictEqual(typeof jadenCase, 'function')
+//assert.strictEqual(jadenCase('str'), 'Str')
+assert.strictEqual(jadenCase("je code a la wild"), "Je Code A La Wild")
+//assert.fail('You must write your own tests')
 // End of tests */
